6ab0f46d2c3840fa95fc6c2cc435f3416de7cd51
rsalvo <rsalvo@roamdata.com>
2017-02-21 06:38:08 -0500

    first pass.

---
+++ Module/Course.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    class Course
+    {
+        private ArrayList students = new ArrayList();
+        private Teacher[] teachers = new Teacher[3];
+
+        //// Course Info
+        public string CourseName { get; private set; }
+        public int Credits { get; private set; }
+        public int Duration { get; private set; }
+
+        public Course(string courseName, int credits, int duration)
+        {
+            CourseName = courseName;
+            Credits = credits;
+            Duration = duration;
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public bool AddTeacher(Teacher teacher)
+        {
+            bool bTeacherAdded = false;
+
+            // Add the teacher if there is room
+            for (int i = 0; i < teachers.Length; i++)
+            {
+                if (teachers[i] == null)
+                {
+                    teachers[i] = teacher;
+                    bTeacherAdded = true;
+                    break;
+                }
+            }
+
+            if (!bTeacherAdded)
+            {
+                Console.WriteLine("Unable to add teacher {0} {1}.  Course is full.", teacher.FirstName, teacher.LastName);
+            }
+            return bTeacherAdded;
+        }
+
+        /// <summary>
+        /// Get the number of students enrolled in the course.
+        /// </summary>
+        public int EnrolledStudentCount {
+            get {
+                int count = 0;
+                foreach(var student in students)
+                {
+                    if (student != null)
+                        count++;
+                }
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// List the students enrolled in the course
+        /// </summary>
+        public void ListStudents()
+        {
+            foreach(Student student in students)
+            {
+                Console.WriteLine("{0} {1}", student.FirstName, student.LastName);
+            }
+        }
+    }
+}
---
+++ Module/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    class Student : Person
+    {
+        private static int enrolledStudents;
+
+        public static int EnrolledStudents {
+            get { return enrolledStudents; }
+        }
+
+        public Student(string first, string last, DateTime date) : base(first, last, date)
+        {
+            enrolledStudents++;
+        }
+
+        public override void PrepareForClass()
+        {
+            Console.WriteLine("Review reading assignments and do homework.");
+        }
+
+        public void TakeTest()
+        {
+            Console.WriteLine("Take test (Yikes).");
+        }
+
+        public void GoToLibrary()
+        {
+            Console.WriteLine("Go to the library.");
+        }
+
+        public override void GoAboutMyDay()
+        {
+            PrintBio();
+            PrepareForClass();
+            TakeTest();
+            GoToLibrary();
+        }
+
+        private Stack grades = new Stack();
+
+        public void AddGrade(int grade)
+        {
+            grades.Push(grade);
+        }
+    }
+}
